191f152a7c1d9949514e8ed3f30ccb2067eb7649
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-12 16:07:43 -0500

    build repo and begin work on converter JS and index

--- converter.js
+++ converter.js
@@ -1,43 +1,34 @@
-console.log("greetings.")
+let input = document.getElementById("input");
+let c = document.getElementById("C");
+let f =  document.getElementById("F");
+let output = document.getElementById("output");
+let converter = document.getElementById("converter");
+let clear = document.getElementById("clear");
 
-// Write a program that will convert a temperature from fahrenheit to celsius, or from celsius to fahrenheit.
+function toCelsius() {
 
-// In the HTML, have one input field where someone can enter in a temperature.
-// Create a radio button group where Celsius or Fahrenheit can be selected as the scale that the number should be converted to.
-// Create a block level element that will hold the text of the converted temperature.
-// Create a button that, when clicked, displays the converted temperature.
-// Create another button that, when clicked, clears any text in the input field.
-// Add an event handler to the input field that checks if the user pressed the enter key, and if that happens, perform the conversion.
-// If the temperature is greater than 90F/32C the color of the converted temperature should be red.
-// If the temperature is less than 32F/0C the color of the converted temperature should be blue.
-// For any other temperature, the color should be green.
+};
 
-function toCelsius () {
+function toFahrenheit() {
 
-}
+};
 
-function toFahrenheit () {
-
-}
-
-// Get a reference to the button element in the DOM
-var button = document.getElementById("converter");
+function clearInput() {
+	input.value = "";
+};
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
-  console.log("event", clickEvent);
-}
+  console.log("event", clickEvent)
+};
 
 // Assign a function to be executed when the button is clicked
-button.addEventListener("click", determineConverter);
-
-
-
-
-
-
-
-
-
+converter.addEventListener("click", determineConverter);
+clear.addEventListener("click", clearInput);
+input.addEventListener("keyup", function(e) {
+	if (e.keyCode === 13) {
+		determineConverter()
+	}
+});
 
