90cc985d61e7ea96a72275b67efaa7f62e470f3b
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-12 21:32:40 -0500

    get converters to work and get clear input to finally work

--- converter.js
+++ converter.js
@@ -5,22 +5,41 @@ let output = document.getElementById("output");
 let converter = document.getElementById("converter");
 let clear = document.getElementById("clear");
 
-function toCelsius() {
-
+function toCelsius(fValue) {
+	// °C = (°F - 32) x 5/9
+	let cValue = (fValue - 32) * (5/9);
+	output.innerHTML = cValue;
+	return cValue;
 };
 
-function toFahrenheit() {
-
+function toFahrenheit(cValue) {
+	// °F = °C x 9/5 + 32
+	let fValue = (cValue * (9 / 5)) + 32
+	output.innerHTML = fValue;
+	return fValue;
 };
 
 function clearInput() {
 	input.value = "";
+	document.getElementById("f").checked = false;
+	document.getElementById("c").checked = false;
+	output.innerHTML = "";
+	// document.getElementById("F").checked = false;
+	// document.getElementById("C").checked = false;
+	// output.innerHTML = "";
+	
 };
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
 function determineConverter (clickEvent) {
   console.log("event", clickEvent)
+  var toConvert = document.getElementById("input").value;
+	if (document.getElementById("f").checked) {
+	  toFahrenheit(toConvert);
+	} else if (document.getElementById("c").checked) {
+	  toCelsius(toConvert);
+	}
 };
 
 // Assign a function to be executed when the button is clicked
